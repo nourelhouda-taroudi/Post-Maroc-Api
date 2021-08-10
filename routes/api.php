9f9d6b9e823f3628d686bc46ee7a10b7f711354e
unknown <nourelhoudataroudi@gmail.com>
2021-08-10 18:30:02 +0200

    [dev] : Updated some routes

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
     Route::post('/sign','CreditController@sign');
 
     // Document routes
-    Route::post('/uploadDocument','DocumentController@storeDocument');
+    Route::post('/{CIN}/uploadDocument','DocumentController@storeDocument');
     Route::get('/{id}/getDocument','DocumentController@getDocument'); // get document for specific client
     Route::put('/{id}/updateDocument','DocumentController@updateDocument');
     Route::get('/getAllDocuments','DocumentController@getAllDocuments'); // 2 cas =>1:getAll for all client 2:getAll for specific client
