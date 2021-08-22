6df046b55996e30ddaf7ad11879162b756eacf26
Nour Elhouda Taroudi <83907681+nourelhouda-taroudi@users.noreply.github.com>
2021-08-22 11:52:02 +0100

    [dev] : Update http response

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -2,70 +2,145 @@
 
 namespace App\Http\Controllers;
 
-use App\Document;
-use App\Client;
+use App\Credit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Spatie\Crypto\Rsa\KeyPair;
+use Spatie\Crypto\Rsa\PrivateKey;
+use Spatie\Crypto\Rsa\PublicKey;
 
-class DocumentController extends Controller
+class CreditController extends Controller
 {
-    public function storeDocument(Request $request,$CIN){
-        // $client=Client::where('CIN_Number','=',$CIN)->get();
-        $client=DB::table('clients')
-                ->where('CIN_Number','=',$CIN)
-                ->select('clients.*')
-                ->first();
-        if($client==null){
-            return response()->json('This CIN is invalid');
+    public function createCredit($CIN , Request $request){
+        $amount = $request->amount;
+        $rate = 14;
+        $monthly = $request->monthly;
+        $min = 10000;
+
+        $salary_std = DB::table('clients')
+                        ->where('CIN_Number','=',$CIN)
+                        ->select('salary')
+                        ->first();
+        // Check if client already exist in database
+        if($salary_std == null){
+            return response()->json(['error'=>"CIN invalid."],422);
         }
-        if($request->files){
-            $destinationPath = 'uploads';
-            if ($request->hasFile('CIN_front')){
-                $CIN_front = $request->file('CIN_front');
-                $CIN_front_filename = time().$CIN_front->getClientOriginalName();
-                $CIN_front->move($destinationPath,$CIN_front_filename);
-                $CIN_front_path=env('APP_URL', 'http://127.0.0.1').":".env('APP_PORT','8000').'/'.$destinationPath.'/'.$CIN_front_filename;
-            }
-
-            if ($request->hasFile('CIN_back')){
-                $CIN_back =  $request->file('CIN_back');
-                $CIN_back_filename=time().$CIN_back->getClientOriginalName();
-                $CIN_back->move($destinationPath,$CIN_back_filename);
-                $CIN_back_path=env('APP_URL', 'http://127.0.0.1').":".env('APP_PORT','8000').'/'.$destinationPath.'/'.$CIN_back_filename;
-            }
-
-            if ($request->hasFile('salaryCertificate')){
-                $salaryCertificate =  $request->file('salaryCertificate');
-                $salaryCertificate_filename=time().$salaryCertificate->getClientOriginalName();
-                $salaryCertificate->move($destinationPath,$salaryCertificate_filename);
-                $salaryCertificate_path=env('APP_URL', 'http://127.0.0.1').":".env('APP_PORT','8000').'/'.$destinationPath.'/'.$salaryCertificate_filename;            
-            }
-
-            if ($request->hasFile('certificateResWaterElec')){
-                $certificateResWaterElec =  $request->file('certificateResWaterElec');
-                $certificateResWaterElec_filename=time().$certificateResWaterElec->getClientOriginalName();
-                $certificateResWaterElec->move($destinationPath,$certificateResWaterElec_filename);
-                $certificateResWaterElec_path=env('APP_URL', 'http://127.0.0.1').":".env('APP_PORT','8000').'/'.$destinationPath.'/'.$certificateResWaterElec_filename;
-            }
-            
-            if ($request->hasFile('jobCertificate')){
-                $jobCertificate =  $request->file('jobCertificate');
-                $jobCertificate_filename=time().$jobCertificate->getClientOriginalName();
-                $jobCertificate->move($destinationPath,$jobCertificate_filename);
-                $jobCertificate_path=env('APP_URL', 'http://127.0.0.1').":".env('APP_PORT','8000').'/'.$destinationPath.'/'.$jobCertificate_filename;
-            }
-
-            $document=new Document();
-            $document->CIN_front=$CIN_front_path;
-            $document->CIN_back=$CIN_back_path;
-            $document->salaryCertificate=$salaryCertificate_path;
-            $document->certificateResWaterElec=$certificateResWaterElec_path;
-            $document->jobCertificate=$jobCertificate_path;
-            $document->client_CIN_Number=$CIN;
-            $document->save();
-            return response()->json($document);
+        // Get salary from Std salary object
+        $salary = $salary_std->salary;
+        if($amount < $min){
+            return response()->json(['error'=>"Amount must be greater than 10000Dh."],422);
         }
+
+        // 1500DH - 4000DH
+        if($salary>=1500 && $salary<4000){
+            $max = 200000;
+            if($amount>=$max){
+                return response()->json(['error'=>"Amount invalid."],422);
+            }
+            $credit = $this->calculateAndCreateCredit($amount,$rate,$CIN,$monthly);
+            return response()->json($credit,201);
+        }
+
+        // 40000DH - 10000DH
+        if($salary >= 4000 && $salary < 10000){
+            $age =  (DB::table('clients')
+                            ->where('CIN_Number','=',$CIN)
+                            ->select('age')
+                            ->first()
+                     )->age;
+
+            if($age<40)$max=600000;
+            if($age>=40)$max=400000;
+            if($amount>=$max){
+                return response()->json(['error'=>"Amount invalid."],422);
+            }
+            $credit = $this->calculateAndCreateCredit($amount,$rate,$CIN,$monthly);
+            return response()->json($credit,201);
+        }
+
+        // 10000DH - 50000DH
+        if($salary >= 10000 && $salary < 50000){
+            $age =  (DB::table('clients')
+                            ->where('CIN_Number','=',$CIN)
+                            ->select('age')
+                            ->first()
+                     )->age;
+
+            if($age<40)$max=1000000;
+            if($age>=40)$max=800000;
+            if($amount>=$max){
+                return response()->json(['error'=>"Amount invalid."],422);
+            }
+            $credit = $this->calculateAndCreateCredit($amount,$rate,$CIN,$monthly);
+            return response()->json($credit,201);
+        }
+        // >50000DH
+        if($salary >= 50000){
+            $age =  (DB::table('clients')
+                            ->where('CIN_Number','=',$CIN)
+                            ->select('age')
+                            ->first()
+                     )->age;
+
+            if($age<40)$max=1500000;
+            if($age>=40)$max=1000000;
+            if($amount>=$max){
+                return response()->json(['error'=>"Amount invalid."],422);
+            }
+            $credit = $this->calculateAndCreateCredit($amount,$rate,$CIN,$monthly);
+            return response()->json($credit,201);
+        }
+        return response()->json(['error'=>"Sorry, Can not create credit."],422);
+    }
+
+    private function calculateAndCreateCredit($amount,$rate,$CIN,$monthly){
+        $duration = ($amount*(1+($rate/100)))/$monthly;
+        $credit = new Credit();
+        $credit->amount = $amount;
+        $credit->rate = $rate;
+        $credit->duration = $duration;
+        $credit->monthly = $monthly;
+        $credit->Client_CIN_Number = $CIN;
+        $credit->save();
+        // Update account balance for client
+        $accountBalance=(DB::table('clients')
+                            ->where('CIN_Number', $CIN)
+                            ->select('accountBalance')
+                            ->first()
+                        )->accountBalance;
         
-        return response()->json(["Aucun fichier entrer"],422);
+        DB::table('clients')
+            ->where('CIN_Number', $CIN)
+            ->update(['accountBalance' => $accountBalance+$amount]);
+
+        return $credit;
+    }
+    public function sign(Request $request , $idCredit){
+ 
+        $credit = Credit::where('idCredit',$idCredit)->first();
+        if($credit== null){
+            return response()->json(['error'=>'Credit is not found.'],404);
+        }
+        // generating an RSA key pair
+        [$privateKey, $publicKey] = (new KeyPair())->generate();
+
+        // Get PDF as base64
+        $file = $request->document;
+
+        // Hash file with SHA256
+        $hash_file = hash('sha256',$file);
+
+        //$data = base64_encode($file);
+
+        // Create signature
+        $signature = PrivateKey::fromString($privateKey)->sign($hash_file);
+        DB::table('credits')
+            ->where('idCredit', $idCredit)
+            ->update([
+            'signature' => $signature,
+            'publicKey' => $publicKey
+      ]);
+
+        return response()->json($signature,201);
     }
 }
